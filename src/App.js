fc45c5ee4932ad84e0ebe643ea942016dff77457
Simone Fiore <simone.fiore@aulab.it>
2021-10-16 10:05:42 +0200

    start routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import Home from "./Components/Views/Home/Home";
+import Test from "./Components/Views/Test/Test";
 
 function App() {
   return (
-    <div className="App">
-      <p>Ciao, sono in homepage</p>
-      <Home />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/test">Test</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/region/:region"></Route>
+          <Route path="/test">
+            <Test />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
